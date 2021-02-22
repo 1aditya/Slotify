007b980019cd848f67173373ded0aee32556b019
1aditya <adityajoshi47@gmail.com>
2021-02-22 20:47:41 +0530

    Add files via upload

---
+++ includes/classes/Constants.php
@@ -0,0 +1,19 @@
+<?php
+class Constants{
+
+    public static $passwordDoNotMatch = "Your password don't match";
+    public static $passwordNotAlphanumeric = "Your password can only contain numbers and letters.";
+    public static $passwordCharachters = "Your Password must be between 5 and 30 charachters.";
+    public static $emailInvalid ="Email is invalid";
+    public static $emailsDoNotMatch ="Your email don't match";
+    public static $emailTaken = "This email is already in use";
+    public static $lastNameCharachters = "Your last name must be between 5 and 25 charachters";
+    public static $firstNameCharachters = "Your first name must be between 5 and 25 charachters";
+    public static $usernameCharachters = "Your username must be between 5 and 25 charachters";
+    public static $usernameTaken = "This username alread exists";
+
+
+    public static $loginFailed = "Username or Password entered is incorrect.";
+}
+
+?>
---
+++ includes/classes/Song.php
@@ -0,0 +1,56 @@
+<?php
+
+    class Song{
+        private $id;
+        private $con;
+        private $mysqliData;
+        private $title;
+        private $artistId;
+        private $albumId;
+        private $genre;
+        private $duration;
+        private $path;
+
+        public function __construct($con,$id){
+            $this->con = $con;
+            $this->id = $id;
+            $query = mysqli_query($this->con,"SELECT * FROM songs WHERE id='$this->id'");
+            $this->mysqiData = mysqli_fetch_array($query);
+            $this->title =  $this->mysqiData['title'];
+            $this->artistId =  $this->mysqiData['artist'];
+            $this->albumId =  $this->mysqiData['album'];
+            $this->genre =  $this->mysqiData['genre'];
+            $this->duration =  $this->mysqiData['duration'];
+            $this->path =  $this->mysqiData['path'];
+        }
+        public function getTitle(){
+            return $this->title;
+        }
+
+        public function getId(){
+            return $this->id;
+        }
+
+        public function getArtist(){
+            return new Artist($this->con,$this->artistId);
+        }
+        public function getAlbum(){
+            return new Album($this->con,$this->albumId);
+        }
+        public function getPath(){
+            return $this->Path;
+        }
+        public function getDuration(){
+            return $this->duration;
+        }
+        public function getMysqliData(){
+            return $this->mysqliData;
+        }
+        public function getGenre(){
+            return $this->genre;
+        }
+        
+
+    }
+
+?>
