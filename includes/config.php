b07c41fe2bbe1b1b47885f3ee1093e5efc6af67e
1aditya <adityajoshi47@gmail.com>
2021-02-25 18:12:14 +0530

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -2,9 +2,9 @@
 	ob_start();
 	session_start();
 
-	$timezone = date_default_timezone_set("Europe/London");
+	$timezone = date_default_timezone_set("Asia/Kolkata");
 
-	$con = pg_connect("host=localhost port=5432 dbname=slotify user=postgres password=aditya");
+	$con = pg_connect(getenv("DATABASE_URL")));
 
 	if($con==false) {
 		echo "Failed to connect: ";
