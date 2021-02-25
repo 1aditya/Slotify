d4e9a30760df10eddb3dc28dcb9a95d931db1650
1aditya <adityajoshi47@gmail.com>
2021-02-25 18:15:48 +0530

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -4,7 +4,7 @@
 
 	$timezone = date_default_timezone_set("Asia/Kolkata");
 
-	$con = pg_connect(getenv("DATABASE_URL")));
+	$con = pg_connect(getenv("DATABASE_URL"));
 
 	if($con==false) {
 		echo "Failed to connect: ";
